65273d85a4edc6729ad4835a24c98f3408755ad7
buyekeobare <buyekeobare@gmail.com>
2023-07-20 22:27:43 +0300

    Updated file

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,26 +1,16 @@
-def validate_password(password):
-    # Check password length
+def validate_password (password):
     if len(password) < 8:
         return False
-
-    # Check for at least one uppercase letter, one lowercase letter, and one digit
-    has_uppercase = any(char.isupper() for char in password)
-    has_lowercase = any(char.islower() for char in password)
-    has_digit = any(char.isdigit() for char in password)
-
-    if not (has_uppercase and has_lowercase and has_digit):
-        return False
-
-    # Check for spaces
-    if ' ' in password:
-        return False
-
-    # If all checks pass, return True
-    return True
-
-# Test cases
-print(validate_password("Password123"))  # True
-print(validate_password("Abcd123"))      # False (length < 8)
-print(validate_password("ABC123"))       # False (missing lowercase letter)
-print(validate_password("abcd123"))      # False (missing uppercase letter)
-print(validate_password("Password 123")) # False (contains space)
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+    for char in password:
+        if char .isspace():
+            return False
+        if char .issupper():
+            has_uppercase = True
+            if char .islower():
+                has_lowercase = True
+                if char .isdigit():
+                    has_digit = True
+                    return has_uppercase and has_lowercase and has_digit    
