059abf18723c776b6b5afc7b8e48d2116b3756e3
buyekeobare <buyekeobare@gmail.com>
2023-07-20 22:34:04 +0300

    Updated file

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,16 +1,19 @@
 def validate_password (password):
     if len(password) < 8:
         return False
+    
     has_uppercase = False
     has_lowercase = False
     has_digit = False
+
     for char in password:
         if char .isspace():
             return False
-        if char .issupper():
+        if char.issupper():
             has_uppercase = True
-            if char .islower():
-                has_lowercase = True
-                if char .isdigit():
-                    has_digit = True
+        if char.islower():
+            has_lowercase = True
+        if char.isdigit():
+            has_digit = True
+            
     return has_uppercase and has_lowercase and has_digit    
