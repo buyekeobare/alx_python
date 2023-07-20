170981a05fe2337c07669ba46b08f4d2561524d0
buyekeobare <buyekeobare@gmail.com>
2023-07-20 21:23:42 +0300

    Updated file

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -6,13 +6,7 @@ def fibonacci_sequence(n):
     while len(fibonacci_numbers) < n:
         next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
         if next_number < 0:
-            return []  # incorrect input
+            return []  # Overflow occurred, cannot generate Fibonacci numbers for n
         fibonacci_numbers.append(next_number)
 
     return fibonacci_numbers[:n]
-
-n = 10
-result = fibonacci_sequence(n)
-print(result)
- 
-print(fibonacci_sequence(5))
