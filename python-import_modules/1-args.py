07a9fa5dcad408eb0b85d1b6abf5d228445dc416
buyekeobare <buyekeobare@gmail.com>
2023-07-25 22:20:30 +0300

    Updated file

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    num_args = len(sys.argv) - 1
+    args = sys.argv[1:]
+
+    if num_args == 0:
+        print("0 arguments.")
+        return
+
+    print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+    for i, arg in enumerate(args, 1):
+        print(f"{i}: {arg}")
+
+if _name_ == "_main_":
+    main()
