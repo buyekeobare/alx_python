8fd694db678a04f0544fe8fbe5d8e828d5d32d1c
buyekeobare <buyekeobare@gmail.com>
2023-08-31 21:50:24 +0300

    Updated files

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,47 @@
+#! usr\bin\python3
+
+'''
+  Write a script that starts a Flask web application:
+'''
+
+from flask import Flask
+
+'''
+  Flask imported
+'''
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+  return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+  return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+  return("C {}".format(text.replace("_", " ")))
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def python(text):
+  return("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<int:n>")
+def number(n):
+  return("{} is a number" .format(n))
+
+@app.route("/number_template/<int:n>")
+def template(n):
+  return(render_template("5-number.html", number=n))
+
+
+
+if __name__=="__main__":
+  app.run(debug=True, port="5000")
